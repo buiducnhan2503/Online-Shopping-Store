e1e62842c54b950cde67ed421031da62d324a0c7
BUIDUCNHAN <buiducnhan.it@gmail.com>
2024-08-29 11:18:45 +0700

    Error Handling And Logging

---
+++ src/main/java/buiducnhan/hutech/Online_Shopping_Store/exception/InsufficientQuantityException.java
@@ -0,0 +1,7 @@
+package buiducnhan.hutech.Online_Shopping_Store.exception;
+
+public class InsufficientQuantityException extends RuntimeException {
+    public InsufficientQuantityException(String message) {
+        super(message);
+    }
+}
